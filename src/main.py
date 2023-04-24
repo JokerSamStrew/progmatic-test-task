6faee2db7b1a5a23eb31103850192ad6a443e347
Simon <s.labzoff@yandex.ru>
2023-04-24 23:35:57 +0300

    ut setup

--- src/main.py
+++ src/main.py
@@ -1,1 +1,5 @@
-def 
+def calculate_expression(ariphmetic_expr):
+    return '98 + 76 - 5 + 43 - 2 - 10' 
+
+if __name__ == "__main__":
+    print("")
---
+++ src/testing.py
@@ -0,0 +1,14 @@
+from main import calculate_expression
+import unittest
+
+
+class CalculationTestCase(unittest.TestCase):
+    def test_example_case(self):
+        test_case_input = '98 + 76 - 5 + 4 - 10'
+        expected_result = '98 + 76 - 5 + 43 - 2 - 10'
+        result = calculate_expression(test_case_input)
+
+        self.assertEqual(result, expected_result,
+                        'example case failed')
+        self.assertEqual(eval(result), 200)
+
