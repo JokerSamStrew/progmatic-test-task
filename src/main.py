20c4b99504e50d92236c1c13671c3dbec59d81c6
Simon <s.labzoff@yandex.ru>
2023-04-26 12:48:55 +0300

    find all valid combinations

--- src/main.py
+++ src/main.py
@@ -1,146 +1,28 @@
-import re
-
-def parse_expression(expr):
-    return re.findall(r'([+-])?\s*(\d+)', expr)
-
-def split_tokens(tokens):
-    for i in range(4, 9):
-        index = 0
-        for op, value in tokens:
-            if value.find(str(i)) != -1:
-                return tokens[:index], tokens[index:] 
-            index += 1
-
 def tokens_sum(tokens):
     return sum([int(op + value) for op, value in tokens])
 
 def tokens_to_str(tokens):
     return ' '.join([op + ' ' + value for op, value in tokens]).strip()
 
-def modify_strategy_1(tail, desired_result):
-    new_tail = []
-    op, value = tail[0]
-    new_tail.append((op, value + '3'))
-    new_tail.append(('-', '2'))
-    new_tail += tail[1:]
+def tokens_variety(tokens, number, result):
+    if number == -1:
+        if tokens_sum(tokens) == 200:
+            result.append(tokens)
+        return
 
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    new_tail = []
-    op, value = tail[0]
-    new_tail.append((op, value + '3'))
-    new_tail.append(('+', '2'))
-    new_tail += tail[1:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    return []
-
-def modify_strategy_2(tail, desired_result):
-    new_tail = []
-    new_tail.append(tail[0])
-    new_tail.append(('+', '3'))
-    new_tail.append(('+', '2'))
-    new_tail += tail[1:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    new_tail = []
-    new_tail.append(tail[0])
-    new_tail.append(('-', '3'))
-    new_tail.append(('+', '2'))
-    new_tail += tail[1:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    new_tail = []
-    new_tail.append(tail[0])
-    new_tail.append(('+', '3'))
-    new_tail.append(('-', '2'))
-    new_tail += tail[1:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    return []
-
-def modify_strategy_3(tail, desired_result):
-    if len(tail) <= 1:
-        return []
-
-    op, value = tail[1]
-    new_tail = []
-    new_tail.append(tail[0])
-    new_tail.append(('+', '3'))
-    new_tail.append((op, '2' + value ))
-    if len(tail) >= 2:
-        new_tail += tail[2:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    new_tail = []
-    new_tail.append(tail[0])
-    new_tail.append(('-', '3'))
-    new_tail.append((op, '2' + value))
-    if len(tail) >= 2:
-        new_tail += tail[2:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    return []
-
-def modify_strategy_4(tail, desired_result):
-    if len(tail) <= 1:
-        return []
-
-    op, value = tail[0]
-    new_tail = []
-    new_tail.append((op, value + '3'))
-    op, value = tail[1]
-    new_tail.append((op, '2' + value ))
-    if len(tail) >= 2:
-        new_tail += tail[2:]
-
-    if tokens_sum(new_tail) == desired_result:
-        return new_tail
-
-    return []
-
-def modify_tail(tail, desired_result):
-    if tokens_sum(tail) == desired_result:
-        return tail
-
-    md_strgs = [modify_strategy_1, 
-                modify_strategy_2,
-                modify_strategy_3,
-                modify_strategy_4]
-
-    for md_strg in md_strgs:
-        result = md_strg(tail, desired_result)
-        if len(result) > 0:
-            return result
-
-    return []
+    tokens_variety(tokens + [('+', str(number))], number - 1, result)
+    tokens_variety(tokens + [('-', str(number))], number - 1, result)
+    op, value = tokens[-1]
+    tokens_variety(tokens[:-1] + [(op, value + str(number))], number - 1, result)
 
 
-def calculate_expression(ariphmetic_expr):
-    tokens = parse_expression(ariphmetic_expr)
-    if len(tokens) == 0:
-       raise Exception("Failed parse expression") 
 
-    head, tail = split_tokens(tokens)
-    head_sum = tokens_sum(head)
-    modified_tail = modify_tail(tail, 200 - head_sum)
-    if len(modified_tail) == 0:
-       raise Exception("Solution not found") 
-
-    return tokens_to_str(head + modified_tail)
+def calculate_expressions():
+    results = []
+    tokens_variety([('', '9')], 8, results) 
+    for result in results:
+        str_result = tokens_to_str(result)
+        print(str_result, ' = ', eval(str_result))
 
 if __name__ == "__main__":
-    print(calculate_expression(input('Enter expression: ')))
+    calculate_expressions()
--- src/testing.py
+++ src/testing.py
@@ -1,33 +1,15 @@
 from main import * 
 import unittest
+import random
 
 
 class CalculationTestCase(unittest.TestCase):
     def test_example_case(self):
-        test_case_input = '98 + 76 - 5 + 4 - 10'
-        expected_result = '98 + 76 - 5 + 43 - 2 - 10'
-        result = calculate_expression(test_case_input)
+        results = []
+        tokens_variety([('', '9')], 8, results) 
 
-        self.assertEqual(result, expected_result,
-                        'example case failed')
-        self.assertEqual(eval(result), 200, 
-                        'expression doesn\'t equal 200')
+        for result in results:
+            str_result = tokens_to_str(result)
+            self.assertEqual(eval(str_result), 200, 
+                        f'expression "{str_result}" doesn\'t equal 200')
 
-        test_case_input = '98 + 76 - 5 + 43 - 2 - 10'
-        result = calculate_expression(test_case_input)
-        self.assertEqual(result, expected_result,
-                        'valid case failed')
-
-    def test_fail_case(self):
-        try:
-            test_case_input = ['98', '98 + 76 - 5 + 43 - 2']
-            calculate_expression(test_case_input)
-        except Exception as ex:
-            self.assertEqual(repr(ex), "Exception('Solution not found')")
-
-
-        try:
-            for test_case_input in ['', 'dsagasd']:
-                calculate_expression(test_case_input)
-        except Exception as ex:
-            self.assertEqual(repr(ex), "Exception('Failed parse expression')")
